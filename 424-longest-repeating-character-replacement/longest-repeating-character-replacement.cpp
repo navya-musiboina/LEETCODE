0ae39f1e99e7e51411097f12c0e8c8153b6b22f4
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-01 19:32:10 +0530

    Time: 4 ms (62.41%) | Memory: 11.4 MB (5.54%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) 
+    {
+        int n=s.size();
+        map<char,int>mpp;
+        int maxi=0,ans=0,j=0;
+        for(int i=0;i<n;i++)
+        {
+            mpp[s[i]]++;
+            maxi=max(maxi,mpp[s[i]]);
+            while(i-j+1-maxi>k)
+            {
+                mpp[s[j]]--;
+                if(mpp[s[j]]==0)
+                mpp.erase(s[j]);
+                j++;
+            }
+            ans=max(ans,i-j+1);
+        }
+        return ans;
+    }
+};
