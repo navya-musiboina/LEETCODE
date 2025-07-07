43ee816541c4dfb7b942427ac76b6d6724fcd9c6
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-07 23:55:07 +0530

    Time: 63 ms (54.50%) | Memory: 74.9 MB (64.96%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -9,22 +9,19 @@ public:
         priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<n;i++)
         maxDay=max(maxDay,events[i][1]);
-          int day;
+          int day=1;
          int index=0,ans=0;
         while(index<n || !pq.empty())
         {
-            if (pq.empty()) {
-                day = events[index][0];
-            }
-
+           
              while(index<n && day>=events[index][0])
            {
              pq.push(events[index][1]);
              index++;
            }
-             while(!pq.empty() && pq.top()<day)
+            while(!pq.empty() && pq.top()<day)
            pq.pop();
-           //need to remove all the elements that are already expired
+        
            if(!pq.empty())
            {
             pq.pop();
