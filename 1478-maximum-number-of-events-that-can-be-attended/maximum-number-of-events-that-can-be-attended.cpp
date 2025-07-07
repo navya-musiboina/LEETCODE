663337b7fc043f3ff9bd069fd8675cdd7d28cd6e
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-07 23:46:15 +0530

    Time: 57 ms (84.03%) | Memory: 74.8 MB (85.96%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+   
+
+    int maxEvents(vector<vector<int>>& events) 
+    {
+        int n=events.size(),maxDay=0;
+        sort(events.begin(),events.end());
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<n;i++)
+        maxDay=max(maxDay,events[i][1]);
+
+         int index=0,ans=0;
+        for(int day=1;day<=maxDay;day++)
+        {
+             while(index<n && day==events[index][0])
+           {
+             pq.push(events[index][1]);
+             index++;
+           }
+             while(!pq.empty() && pq.top()<day)
+           pq.pop();
+           //need to remove all the elements that are already expired
+           if(!pq.empty())
+           {
+            pq.pop();
+            ans++;
+           }
+         
+
+        }
+        return ans; 
+    }
+};
