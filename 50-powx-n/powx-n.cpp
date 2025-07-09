af17ac36b3dc3d3ed81a133c5c1d45d721eff766
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-09 12:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.34%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -13,27 +13,27 @@ public:
     {
         long long nn=n;
         if(n<0)
-         nn=(long long)-1*n;
-        double ans=1.0;
-
+        nn=-nn;
+        double ans=1;
         while(nn>0)
         {
             if(nn%2==0)
             {
-                x=x*x;
                 nn=nn/2;
+                x=x*x;
             }
             else
             {
+                nn=nn-1;
                 ans=ans*x;
-                nn--;
             }
         }
-        if(n<0)
-        return (double)1/ans;
-
-        else
+        if(n>0)
         return ans;
+        else
+        return (double)1.0/(ans);
+
+
 
     }
 };
