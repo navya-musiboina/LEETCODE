76298b79941c055324bc66fdeaf5c6cec13fedfc
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-15 10:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (74.82%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-     void func(string digits,string output,int i,vector<string>&res,string mpp[])
+     void func(string mpp[],string &ans,vector<string>&res,int i,string digits)
      {
-        if(i==digits.size())
-        {
-            res.push_back(output);
-            return;
-        }
-        int num=digits[i]-'0';
-        string value=mpp[num];
-        for(int j=0;j<value.size();j++)
-        {
-            output.push_back(value[j]);
-            func(digits,output,i+1,res,mpp);
-            output.pop_back();
-        }
+       if(i >= digits.size())
+       {
+        res.push_back(ans);
+        return ;
+       }
+       int number=digits[i]-'0';
+       string sub=mpp[number];
+       for(int j=0;j<sub.size();j++)
+       {
+        ans+=sub[j];
+        func(mpp,ans,res,i+1,digits);
+        ans.pop_back();
+       }
+
+
+
      }
     vector<string> letterCombinations(string digits) 
     {
-        vector<string>res;
-        string output;
+      vector<string>res;
+      string ans;
+      
         if(digits.size()==0)
-        return res;
-        int index=0;
-        string mpp[10]={"", "", "abc","def","ghi","jkl", "mno", "pqrs", "tuv", "wxyz"};
-        func(digits,output,index,res,mpp);
-        return res;
+        return  res;
+      string mpp[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+      func(mpp,ans,res,0,digits);
+      return res;
     }
 };
