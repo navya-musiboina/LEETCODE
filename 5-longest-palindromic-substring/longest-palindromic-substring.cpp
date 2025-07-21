b76a139ad3666380d57132a8c6bbd8c47e92d875
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-21 23:47:16 +0530

    Time: 94 ms (36.61%) | Memory: 174.1 MB (16.63%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    string check(string s,int i,int j)
+    {
+      while(i>=0 && j<s.size() && s[i]==s[j])
+      {
+        i--;
+        j++;
+      }
+      return s.substr(i+1,(j-i-1));
+    }
+    string longestPalindrome(string s) 
+    {
+          string res="";
+        for(int i=0;i<s.size();i++)
+        {
+           string a= check(s,i,i);
+           string b= check(s,i,i+1);
+           if(a.size()>b.size() && res.size()<a.size())
+           res=a;
+           else if(a.size()<b.size() && res.size()<b.size())
+           res=b;
+        }
+        return res;
+        
+    }
+};
