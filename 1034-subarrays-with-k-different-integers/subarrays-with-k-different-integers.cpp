0d50a661b1481a7fdfb342db789aefd1df32002b
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-07-27 21:20:44 +0530

    Time: 171 ms (5.47%) | Memory: 67.4 MB (9.98%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-     
-    int atleast(vector<int>&nums,int k)
-    {
+     int atleast(vector<int>&nums,int k)
+     {
         map<int,int>mpp;
-        int j=0,ans=0;
-        for(int i=0;i<nums.size();i++)
+        int j=0,count=0,n=nums.size();
+        for(int i=0;i<n;i++)
         {
             mpp[nums[i]]++;
             while(mpp.size()>=k)
             {
-            ans+=(nums.size()-i);
-            mpp[nums[j]]--;
-            if(mpp[nums[j]]==0)
-            mpp.erase(nums[j]);
-            j++;
+                count+=n-i;
+                mpp[nums[j]]--;
+                if(mpp[nums[j]]==0)
+                mpp.erase(nums[j]);
+                j++;
             }
         }
-        return ans;
-    }
+        return count;
+     }
     int subarraysWithKDistinct(vector<int>& nums, int k) 
-    {
-        int n=nums.size();
+    { 
         return atleast(nums,k)-atleast(nums,k+1);
-        
     }
 };
