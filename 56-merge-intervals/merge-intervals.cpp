fced6ed18d68d83cfa3e7ddda25156b74d9c28e8
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-06 10:04:52 +0530

    Time: 4 ms (69.60%) | Memory: 23.7 MB (80.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-       sort(intervals.begin(),intervals.end());
-       vector<vector<int>>ans;
-       int start=intervals[0][0];
-       int end=intervals[0][1];
-       for(int i=1;i<intervals.size();i++)
-       {
+    int n=intervals.size();
+     sort(intervals.begin(),intervals.end());
+     int start=intervals[0][0];
+     int end=intervals[0][1];
+     vector<vector<int>>ans;
+     for(int i=1;i<n;i++)
+     {
         if(end>=intervals[i][0])
         end=max(end,intervals[i][1]);
         else
@@ -16,8 +17,9 @@ public:
             start=intervals[i][0];
             end=intervals[i][1];
         }
-       }
-       ans.push_back({start,end});
-       return ans;
+     }
+     ans.push_back({start,end});
+     return ans;
+
     }
 };
