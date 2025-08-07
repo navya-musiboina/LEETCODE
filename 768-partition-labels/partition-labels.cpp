529e81899654219de14a0e90d7b20052280744b1
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-07 12:03:33 +0530

    Time: 2 ms (38.48%) | Memory: 9 MB (24.69%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -3,27 +3,24 @@ public:
     vector<int> partitionLabels(string s) 
     {
         int n=s.size();
-        map<char,int>mpp;
-        for(int i=0;i<n;i++)
-        mpp[s[i]]=i;
-        int start=0;
-        int end=mpp[s[0]];
-     vector<int>ans;
-        for(int i=0;i<n;i++)
+       map<char,int>mpp;
+       for(int i=0;i<n;i++)
+       mpp[s[i]]=i;
+       vector<int>ans;
+       int end=0,start=0;
+       for(int i=0;i<n;i++)
+       {
+        if(end<i)
         {
-            if(end<i)
-            {
-                ans.push_back(end-start+1);
-                start=i;
-                end=mpp[s[i]];
-            }
-            else
-            {
-                end=max(end,mpp[s[i]]);
-            }
-        }
         ans.push_back(end-start+1);
-        return ans;
-        
+        end=mpp[s[i]];
+        start=i;
+         }
+       else
+        end=max(end,mpp[s[i]]);
+       }
+       ans.push_back(end-start+1);
+       return ans;
+
     }
 };
