4aa393269a2c84f42a9a2c0916339086a972fa0c
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-08 23:21:44 +0530

    Time: 13 ms (44.35%) | Memory: 17.5 MB (41.61%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -3,14 +3,12 @@ public:
    
     TreeNode *func(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        if( root==NULL||  root==p || root==q)
-        return root;
-
+        if(root==NULL) return root;
+        if(root ==p || root == q) return root;
         TreeNode *lh=func(root->left,p,q);
         TreeNode *rh=func(root->right,p,q);
         if(lh && rh) return root;
-        if(lh) return lh;
-        return rh;
+        return lh==NULL?rh:lh;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
