fabb299e538222941905c11a747ed2015c7a2383
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-10 14:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (76.94%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
  //total-min(subarray);
-   int kadanes(vector<int>&nums)
-   {
-    int n=nums.size();
-    int sum=0,maxi=INT_MIN;
-    for(int i=0;i<n;i++)
-    {
-        sum+=nums[i];
-        maxi=max(maxi,sum);
-        if(sum<0)
-        sum=0;
-    }
-    return maxi;
-   }
-   int minikadanes(vector<int>&nums)
-   {
-        int n=nums.size();
-    int sum=0,mini=INT_MAX;
-    for(int i=0;i<n;i++)
-    {
-        sum+=nums[i];
-        mini=min(mini,sum);
-        if(sum>0)
-        sum=0;
-    }
-    return mini;
-   }
+ 
 
+    int kadanes(vector<int>&nums)
+    {
+        int sum=0,maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            maxi=max(maxi,sum);
+            if(sum<0)
+            sum=0;
+        }
+        return maxi;
+    }
+    int minikadanes(vector<int>&nums)
+    {
+        int sum=0,mini=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            mini=min(mini,sum);
+            if(sum>0)
+            sum=0;
+        }
+        return mini;
+    }
     int maxSubarraySumCircular(vector<int>& nums) 
     {
         int n=nums.size();
         int total=accumulate(nums.begin(),nums.end(),0);
         int maxi=kadanes(nums);
         int mini=minikadanes(nums);
-        if(maxi<0)return maxi;
-        return max(maxi,total-mini);
+        if(maxi<0) return maxi;
+        return max(total-mini,maxi);
     }
 };
