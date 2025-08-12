d69613f861bdddea5df0a3000d61790d660de2f9
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-12 21:38:41 +0530

    Time: 1 ms (45.08%) | Memory: 16.4 MB (40.80%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int func(vector<vector<int>>&grid,int i,int j,int n,int m,vector<vector<int>>&dp)
+   
+       
+    int func(vector<vector<int>>& grid,int i,int j, int n,int m,vector<vector<int>>&dp)
     {
         if(i>=n || j>=m) return INT_MAX;
-        if(i==n-1 && j==m-1) return grid[i][j];
+        if(i == n-1 && j == m-1) return grid[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        int lh=func(grid,i+1,j,n,m,dp);
-        int rh=func(grid,i,j+1,n,m,dp);
-        return  dp[i][j]= grid[i][j]+ min(lh,rh);
+
+        return dp[i][j]=grid[i][j]+min(func(grid,i+1,j,n,m,dp),func(grid,i,j+1,n,m,dp));
+
     }
     int minPathSum(vector<vector<int>>& grid) 
     {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(grid,0,0,n,m,dp);
+       int n=grid.size();
+       int m=grid[0].size();
+       vector<vector<int>>dp(n,vector<int>(m,-1));
+       return func(grid,0,0,n,m,dp);
     }
 };
