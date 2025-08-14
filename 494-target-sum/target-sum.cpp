15005886829c1f29d7e2953df522c71f1e01fb4f
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-14 11:50:05 +0530

    Time: 899 ms (10.31%) | Memory: 11.7 MB (82.78%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int func(vector<int>&nums,int sum,int i,int target)
+    int func(vector<int>&nums,int i,int sum,int target)
     {
-       
-       if(i>=nums.size()) 
-       return target==sum;
-       int pos=0,neg=0;
-       pos=func(nums,sum+nums[i],i+1,target);
-       neg=func(nums,sum-nums[i],i+1,target);
-       return pos+neg;
+      
+        if(i>=nums.size())
+        {
+            return target==sum;
+        }
+        int pos=0,neg=0;
+        pos=func(nums,i+1,sum+nums[i],target);
+        neg=func(nums,i+1,sum-nums[i],target);
+        return pos+neg;
       
     }
     int findTargetSumWays(vector<int>& nums, int target) 
     {
-
-        return func(nums,0,0,target);
-        
+        int n=nums.size();
+        int k=func(nums,0,0,target);
+        return k;
     }
 };
