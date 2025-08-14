6a80b1fbad0755fcc043b394552a24c142a61526
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-14 12:34:57 +0530

    Time: 52 ms (5.45%) | Memory: 27.7 MB (6.02%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,13 +7,13 @@ public:
         if(text1[i]==text2[j])
         return dp[i][j]= 1+func(text1,text2,i+1,j+1,n,m,dp);
         else
-            return dp[i][j]=max(func(text1,text2,i+1,j,n,m,dp),func(text1,text2,i,j+1,n,m,dp));
+        return dp[i][j]=max(func(text1,text2,i+1,j,n,m,dp),func(text1,text2,i,j+1,n,m,dp));
     }
     int longestCommonSubsequence(string text1, string text2) 
     {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>>dp(n,vector<int>(m,-1));
         return func(text1,text2,0,0,n,m,dp);
     }
 };
