a9a53165ecf79a09fff45eba2179cb3b6cc5b729
Navii <143500871+navya-musiboina@users.noreply.github.com>
2025-08-15 10:09:41 +0530

    Time: 7 ms (21.60%) | Memory: 22 MB (18.62%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int func(vector<int>&prices,int i,int buy,vector<vector<int>>&dp)
-    {
+   
+     int func(vector<int>&prices,int i,int buy,vector<vector<int>>&dp)
+     {
         if(i>=prices.size()) return 0;
         if(dp[i][buy]!=-1) return dp[i][buy];
         if(buy)
-        {
-            int take=0,ntake=0;
-            take=func(prices,i+1,!buy,dp)-prices[i];
-            ntake=func(prices,i+1,buy,dp);
-            return  dp[i][buy]=max(take,ntake);
-        }
+            return dp[i][buy]= max(func(prices,i+1,!buy,dp)-prices[i],func(prices,i+1,buy,dp));
         else
-        {
-            int take=0,ntake=0;
-            take=func(prices,i+1,!buy,dp)+prices[i];
-            ntake=func(prices,i+1,buy,dp);
-            return  dp[i][buy]=max(take,ntake);
-        }
-    }
+        return dp[i][buy]= max(func(prices,i+1,!buy,dp)+prices[i],func(prices,i+1,buy,dp));
+     }
     int maxProfit(vector<int>& prices) 
     {
         int n=prices.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
-        return func(prices,0,1,dp);
+      return func(prices,0,1,dp);
     }
 };
